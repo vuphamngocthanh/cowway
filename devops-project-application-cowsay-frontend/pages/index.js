8b4b831d4d822379434095e159336cbe1bafdd3d
THANHDEV <ThanhDev>
2023-06-25 17:40:51 +0700

    delete link

--- devops-project-application-cowsay-frontend/pages/index.js
+++ devops-project-application-cowsay-frontend/pages/index.js
@@ -52,7 +52,7 @@ export default function Home() {
         </button>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+      {/* <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://github.com/devopsway/devops-project-application-cowsay-frontend"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -80,7 +80,7 @@ export default function Home() {
             </span>
           </h2>
         </a>
-      </div>
+      </div> */}
     </main>
   );
 }
